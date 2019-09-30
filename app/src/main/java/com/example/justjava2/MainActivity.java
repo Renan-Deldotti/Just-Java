227a5c521836d2bf95e7f089d544d4318899f937
Renan_Deldotti <renan.deldotti@hotmail.com>
2019-09-30 19:48:00 -0300

    Cups Option
    Added an option where the user can choose between using his own paper cup or buying a new cup.

--- app/src/main/java/com/example/justjava2/MainActivity.java
+++ app/src/main/java/com/example/justjava2/MainActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
 
 import java.text.NumberFormat;
@@ -16,16 +18,27 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
     public void submitOrder(View view) {
+        RadioGroup cupBtn = (RadioGroup) findViewById(R.id.cupButtons);
+        int selectRadio = cupBtn.getCheckedRadioButtonId();
+        RadioButton selectedRadBtn = (RadioButton) findViewById(selectRadio);
+        //RadioButton selectedRadBtn = (RadioButton) findViewById(cupBtn.getCheckedRadioButtonId());
         int numOfCoffee = 2;
-        int coffeePrice = 5;
-        display(numOfCoffee);
-        displayPrice(numOfCoffee * coffeePrice);
+        double coffeePrice = 5.25;
+        String radioBtnIdText = selectedRadBtn.getText().toString();
+        if (radioBtnIdText.equals("I need a new cup")){
+            display(numOfCoffee);
+            displayPrice(numOfCoffee * coffeePrice);
+        }else if (radioBtnIdText.equals("I got a paper cup")){
+            coffeePrice = 3.00;
+            display(numOfCoffee);
+            displayPrice(numOfCoffee * coffeePrice);
+        }
     }
     private void display(int number) {
         TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
         quantityTextView.setText("" + number);
     }
-    private void displayPrice(int i){
+    private void displayPrice(double i){
         TextView priceTxtView = findViewById(R.id.price_text_view);
         priceTxtView.setText(NumberFormat.getCurrencyInstance().format(i));
     }
