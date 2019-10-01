0bd370ceea0c83e3ba79e80258efdabc3dd0beff
Renan_Deldotti <renan.deldotti@hotmail.com>
2019-09-30 22:44:26 -0300

    Update Main Activity
    Now the app got an improvement and the user can choose the amount of coffee, cup type and make an order.

--- app/src/main/java/com/example/justjava2/MainActivity.java
+++ app/src/main/java/com/example/justjava2/MainActivity.java
@@ -8,6 +8,8 @@ import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
 
+import org.w3c.dom.Text;
+
 import java.text.NumberFormat;
 
 public class MainActivity extends AppCompatActivity {
@@ -18,28 +20,63 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
     public void submitOrder(View view) {
+        int qntOfCoffee;
+        double coffeePrice = 5.25;
         RadioGroup cupBtn = (RadioGroup) findViewById(R.id.cupButtons);
         int selectRadio = cupBtn.getCheckedRadioButtonId();
         RadioButton selectedRadBtn = (RadioButton) findViewById(selectRadio);
         //RadioButton selectedRadBtn = (RadioButton) findViewById(cupBtn.getCheckedRadioButtonId());
-        int numOfCoffee = 2;
-        double coffeePrice = 5.25;
+        TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
+        if (valNumCoffee(quantityTextView) == 0 || valNumCoffee(quantityTextView) == 1){
+            qntOfCoffee = Integer.parseInt(quantityTextView.getText().toString().trim());
+        }else {
+            quantityTextView.setText(0);
+            qntOfCoffee = 0;
+        }
         String radioBtnIdText = selectedRadBtn.getText().toString();
         if (radioBtnIdText.equals("I need a new cup")){
-            display(numOfCoffee);
-            displayPrice(numOfCoffee * coffeePrice);
+            displayPrice(qntOfCoffee * coffeePrice);
         }else if (radioBtnIdText.equals("I got a paper cup")){
             coffeePrice = 3.00;
-            display(numOfCoffee);
-            displayPrice(numOfCoffee * coffeePrice);
+            displayPrice(qntOfCoffee * coffeePrice);
         }
     }
-    private void display(int number) {
-        TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
-        quantityTextView.setText("" + number);
+    private int valNumCoffee(TextView textView){
+        int i = Integer.parseInt(textView.getText().toString().trim());
+        if (i < 0){
+            return -1;
+        }else if(i==0){
+            return 0;
+        }else if(i>0){
+            return 1;
+        }else{
+            return -1;
+        }
     }
     private void displayPrice(double i){
         TextView priceTxtView = findViewById(R.id.price_text_view);
         priceTxtView.setText(NumberFormat.getCurrencyInstance().format(i));
+    }
+
+    public void increaseQuantity(View view) {
+        TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
+        if (valNumCoffee(quantityTextView) == 0 || valNumCoffee(quantityTextView) == 1){
+            int qntOfCoffee = Integer.parseInt(quantityTextView.getText().toString().trim());
+            qntOfCoffee++;
+            quantityTextView.setText(""+qntOfCoffee);
+        }else{
+            quantityTextView.setText("0");
+        }
+    }
+
+    public void decreaseQuantity(View view) {
+        TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
+        if (valNumCoffee(quantityTextView) == 1){
+            int qntOfCoffee = Integer.parseInt(quantityTextView.getText().toString().trim());
+            qntOfCoffee--;
+            quantityTextView.setText(""+qntOfCoffee);
+        }else {
+            quantityTextView.setText("0");
+        }
     }
 }
